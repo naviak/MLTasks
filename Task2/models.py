f3e635cf96766a9da0c575280f7ae63e4dbfc573
naviak <m.dudar@g.nsu.ru>
2021-10-30 20:35:32 +0300

    crossval and svm

--- Task2/models.py
+++ Task2/models.py
@@ -1,29 +1,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
 
 class MNISTClassifier():
-    def __init__(self, model, X_train, y_train, X_test, y_test):
+    def __init__(self, model, X, y, test_size=0.25, random_state=2, fitting = True):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
         self.model = model
-        self.model.fit(X_train, y_train)
-        self.predictions = self.model.predict(X_test)
-        self.modelScore = self.model.score(X_test, y_test)
-        self.wrongArgs = np.where(self.predictions != y_test)[0]
+        
+        if fitting:
+            self.model.fit(X_train, y_train)
+            self.predictions = self.model.predict(X_test)
+            self.modelScore = self.model.score(X_test, y_test)
         
         self.y_test = y_test
         self.X_test = X_test
+        self.X = X
+        self.y = y
+        self.fitted = fitting
 
     
     def showWrongOnes(self, num):
-        _, ax = plt.subplots(2, 5)
-        ax = ax.flatten()
-        for i in range(10):
-            im_idx = np.where((self.y_test == i) & (self.y_test != self.predictions))[0][num]
-            plottable_image = np.reshape(self.X_test[im_idx], (28, 28))
-            ax[i].imshow(plottable_image, cmap='gray_r')
-            ax[i].title.set_text(f"pred {self.predictions[im_idx]}")
+        if self.fitted:
+            _, ax = plt.subplots(2, 5)
+            ax = ax.flatten()
+            for i in range(10):
+                im_idx = np.where((self.y_test != self.predictions))[0][i]
+                plottable_image = np.reshape(self.X_test[im_idx], (28, 28))
+                ax[i].imshow(plottable_image, cmap='gray_r')
+                ax[i].title.set_text(f"pred {self.predictions[im_idx]}")
             
             
+    def getCrossValidateScore(self):
+        return cross_val_score(self.model, self.X, self.y, cv=5)
+            
+
 def showMnistExamples(X, y, num):
     _, ax = plt.subplots(2, 5)
     ax = ax.flatten()
